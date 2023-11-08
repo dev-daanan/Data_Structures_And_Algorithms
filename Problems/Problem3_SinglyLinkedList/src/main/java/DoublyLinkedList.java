7680129f63c15e124d3be1315a19a80af86527c8
Daanan Dasilva <daanandasilva@gmail.com>
2023-11-08 12:33:57 -0700

    Problem 3 doublyLinkedList started and new Node class created

---
+++ Problems/Problem3_SinglyLinkedList/src/main/java/DoublyLinkedList.java
@@ -0,0 +1,13 @@
+package Problems.Problem3_SinglyLinkedList.src.main.java;
+
+public class DoublyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+
+}
--- Problems/Problem3_SinglyLinkedList/src/main/java/DoublyNode.java
+++ Problems/Problem3_SinglyLinkedList/src/main/java/DoublyNode.java
@@ -3,12 +3,21 @@ package Problems.Problem3_SinglyLinkedList.src.main.java;
 public class Node<T> {
     private T data;
     private Node<T> next;
+    private Node<T> prev;
 
     public Node(T data) {
         this.data = data;
         this.next = null;
+        this.prev = null;
     }
 
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
 
     public T getData() {
         return data;
