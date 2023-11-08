1a8a5a43e6be55a1d1952268a482c981a17d5733
Daanan Dasilva <daanandasilva@gmail.com>
2023-11-08 11:25:37 -0700

    Problem 2 finished

--- Problems/Problem2_Queue/src/main/java/ArrayListQueue.java
+++ Problems/Problem2_Queue/src/main/java/ArrayListQueue.java
@@ -3,6 +3,39 @@ package Problems.Problem2_Queue.src.main.java;
 import java.util.LinkedList;
 
 public class ArrayListQueue<T> {
-    private LinkedList<T> queue;
+    private final LinkedList<T> queue;
 
+    public ArrayListQueue() {
+        this.queue = new LinkedList<>();
+    }
+
+    public void enqueue(T item) {
+        queue.addLast(item);
+    }
+
+    public T dequeue() {
+        if (size() > 0) {
+            return queue.removeFirst();
+        } else {
+            System.out.println("Queue is empty. Returning null.");
+            return null;
+        }
+    }
+
+    public T front() {
+        if (size() > 0) {
+            return queue.getFirst();
+        } else {
+            System.out.println("Queue is empty. Returning null.");
+            return null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
 }
--- Problems/Problem2_Queue/src/main/java/Main.java
+++ Problems/Problem2_Queue/src/main/java/Main.java
@@ -1,4 +1,32 @@
 package Problems.Problem2_Queue.src.main.java;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrayListQueue<Integer> myQueue = new ArrayListQueue<>();
+
+        System.out.println("isEmpty: " + myQueue.isEmpty());
+
+        System.out.println("size: " + myQueue.size());
+
+        myQueue.enqueue(5);
+        myQueue.enqueue(66);
+        myQueue.enqueue(951);
+
+        System.out.println("isEmpty: " + myQueue.isEmpty());
+
+        System.out.println("size: " + myQueue.size());
+
+        System.out.println("Dequeued: " + myQueue.dequeue());
+        System.out.println("Dequeued: " + myQueue.dequeue());
+        System.out.println("Dequeued: " + myQueue.dequeue());
+        System.out.println("Dequeued: " + myQueue.dequeue());
+
+        System.out.println("isEmpty: " + myQueue.isEmpty());
+
+        System.out.println("size: " + myQueue.size());
+
+
+    }
+
+
 }
